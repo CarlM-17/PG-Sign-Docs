aeffb58d16e884bb69eb3ca542a869be20890a2e
CarlM-17 <cmarianojr17@yahoo.com>
2026-09-14 14:08:20 +0800

    Delete public directory

--- public/js/config.js
+++
@@ -1,6 +0,0 @@
-// PG Docs Sign - Supabase browser config
-// The anon key is safe to expose (RLS protects data).
-window.PGDS_CONFIG = {
-  SUPABASE_URL: 'https://icljtvutpjhxlnygxqbg.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImljbGp0dnV0cGpoeGxueWd4cWJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2ODUxNzEsImV4cCI6MjEwMjI2MTE3MX0.c4VKSusY-sukSEzy6mD_lDnyXpad30idGjo5yohvWng'
-};
